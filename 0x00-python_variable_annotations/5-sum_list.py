3559d8067290dc009b08f0bc03c5096c66b4fd76
Kelechi <ebirikele@gmail.com>
2024-08-15 22:02:20 +0100

    Sum list of float numbers

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""List of float Module"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Sum flaot number """
+    return sum(input_list)
