186014a00eccca52229b4c81086345ed52f9cc08
Kelechi <ebirikele@gmail.com>
2024-08-15 22:22:12 +0100

    Union

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python/env python3
+""" Mixed list Module"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Sum Mixed list
+    """
+    return float(sum(mxd_lst))
