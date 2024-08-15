1e34d3ffc1382d6d8cc0071edf8d769cdf0ceac5
Kelechi <ebirikele@gmail.com>
2024-08-15 16:01:39 +0100

    add comment to function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
+    """ Sum up 2 float numbers
+    args:
+        a: float
+        b: float
+    Return: a + b
+    """
     return a + b
